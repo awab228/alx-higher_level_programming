3976081ca0b94a7ff7569523856fce411b8a0246
awab228 <awab228@yahoo.com>
2024-03-02 03:36:56 +0200

    fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to causes an specific response
-curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:School" -d "user_id=98"
+curl -sL 0.0.0.0:5000/catch_me_2 -X PUT -H "Origin:School" -d "user_id=98"
